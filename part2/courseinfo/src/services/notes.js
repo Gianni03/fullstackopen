ed99a6255db573e3508cce3b75512034c448d1af
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-11-29 14:28:04 -0300

    change base url, for backend

--- part2/courseinfo/src/services/notes.js
+++ part2/courseinfo/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -21,8 +21,8 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { 
+export default {
   getAll,
-  create, 
-  update 
+  create,
+  update
 }
--- part3/index.js
+++ part3/index.js
@@ -77,3 +77,4 @@ const PORT = 3001
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 })
+
